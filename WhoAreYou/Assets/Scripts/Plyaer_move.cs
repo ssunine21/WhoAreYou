2dedc714d0b419989f85a4c2d3010654f0704ecb
ssunine21 <xcvbnm757@naver.com>
2018-02-08 14:42:07 +0900

    unity first

---
+++ WhoAreYou/Assets/Scripts/Plyaer_move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plyaer_move : MonoBehaviour
+{
+    Rigidbody2D Rigid2D;
+
+    public float JumpForce = 600.0f;
+    public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.Rigid2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            this.transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            this.transform.Translate(-speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            this.Rigid2D.AddForce(transform.up * this.JumpForce);
+	}
+}
---
+++ WhoAreYou/Assets/Scripts/angle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class angle : MonoBehaviour {
+
+    public float movePower = 500.0f;
+
+    Rigidbody2D rigid;
+
+    Vector3 movement;
+    bool isJumping = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            moveVelocity = Vector3.right;
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            moveVelocity = Vector3.left;
+        }
+
+
+        transform.Rotate(moveVelocity * movePower * Time.deltaTime * 80f);
+    }
+}
