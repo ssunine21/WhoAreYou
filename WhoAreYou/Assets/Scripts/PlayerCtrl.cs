07d18925f79f004dbb0a3bbe17c3fb0ae4d84d13
moongeonsu <ssunine21@gmail.com>
2019-01-01 10:44:25 +0900

    #1228 캐릭터 움직임

---
+++ WhoAreYou/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+	public float moveSpeed = 5.0f;
+
+	private float h = 0.0f;
+	private Transform tr;
+	private Animator animator;
+	private SpriteRenderer render;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		tr = GetComponent<Transform>();
+		animator = GetComponent<Animator>();
+		render = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		h = Input.GetAxis("Horizontal");
+
+		if ( h != 0 ) {
+			animator.SetBool("isRun", true);
+			tr.Translate(Vector2.right * moveSpeed * h * Time.deltaTime, Space.Self);
+			if ( h < 0 )
+				render.flipX = true;
+			else
+				render.flipX = false;
+		}
+		else {
+			animator.SetBool("isRun", false);
+		}
+		Debug.Log("h = " + h.ToString());
+
+    }
+
+
+}
