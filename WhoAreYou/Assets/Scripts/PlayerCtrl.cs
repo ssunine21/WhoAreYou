764e9e309c428045cbc5a694f85af5dcf014e4a9
moongeonsu <ssunine21@gmail.com>
2019-01-04 20:58:34 +0900

    #0104 Late to Friday Worship Add to player Jump Action

--- WhoAreYou/Assets/Scripts/PlayerCtrl.cs
+++ WhoAreYou/Assets/Scripts/PlayerCtrl.cs
@@ -5,27 +5,50 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour
 {
 	public float moveSpeed = 5.0f;
+	public float jumpPower = 10.0f;
+
+	private bool isJump = false;
 
 	private float h = 0.0f;
 	private Transform tr;
 	private Animator animator;
 	private SpriteRenderer render;
+	private Rigidbody2D rigid;
 
-    // Start is called before the first frame update
-    void Start()
+	private readonly int hashMove = Animator.StringToHash("isMove");
+	private readonly int hashJump = Animator.StringToHash("isJump");
+	private readonly int hashdoJump = Animator.StringToHash("doJump");
+
+
+	// Start is called before the first frame update
+	void Start()
     {
 		tr = GetComponent<Transform>();
 		animator = GetComponent<Animator>();
 		render = GetComponent<SpriteRenderer>();
+		rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		h = Input.GetAxis("Horizontal");
+		h = Input.GetAxisRaw("Horizontal");
 
+		Debug.Log("h = " + h.ToString());
+
+    }
+
+	private void FixedUpdate() {
+		Move();
+
+		if ( Input.GetButtonDown("Jump") ){
+			Jump();
+		}
+	}
+
+	private void Move() {
 		if ( h != 0 ) {
-			animator.SetBool("isRun", true);
+			animator.SetBool(hashMove, true);
 			tr.Translate(Vector2.right * moveSpeed * h * Time.deltaTime, Space.Self);
 			if ( h < 0 )
 				render.flipX = true;
@@ -33,11 +56,24 @@ public class PlayerCtrl : MonoBehaviour
 				render.flipX = false;
 		}
 		else {
-			animator.SetBool("isRun", false);
+			animator.SetBool(hashMove, false);
 		}
-		Debug.Log("h = " + h.ToString());
+	}
 
-    }
+	private void Jump() {
+		if ( isJump )
+			return;
 
+		isJump = true;
+		animator.SetBool(hashJump, true);
+		animator.SetTrigger(hashdoJump);
+		rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+	}
 
+	private void OnTriggerEnter2D( Collider2D other ) {
+		if(other.gameObject.layer == 8 && this.rigid.velocity.y < 0 ) {
+			isJump = false;
+			animator.SetBool(hashJump, false);
+		}
+	}
 }
